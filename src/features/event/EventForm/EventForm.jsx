662b0083faa6f4d52948b38ac756e051f0323a1e
areeinwah <areeinwah@gmail.com>
2019-10-06 16:01:28 +0800

    End of section 4

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,42 +1,89 @@
-import React, { Component } from 'react'
-import { Form, Segment, Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Form, Segment, Button } from "semantic-ui-react";
 
 class EventForm extends Component {
-    render() {
-        const { cancelOpenedForm } = this.props
-        return (
-                  <Segment>
-                    <Form>
-                      <Form.Field>
-                        <label>Event Title</label>
-                        <input placeholder="First Name" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>City</label>
-                        <input placeholder="City event is taking place" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
-                      </Form.Field>
-                      <Form.Field>
-                        <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
-                      </Form.Field>
-                      <Button positive type="submit">
-                        Submit
-                      </Button>
-                      <Button
-                       type="button"
-                       onClick={cancelOpenedForm} 
-                      >Cancel</Button>
-                    </Form>
-                  </Segment>
-        )
-    }
+  state = {
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: ""
+  };
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+ 
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    this.props.createEvent(this.state)
+  }
+  
+
+  render() {
+    const { title, date, city, venue, hostedBy } = this.state;
+    const { cancelOpenedForm } = this.props;
+    return (
+      <Segment>
+        <Form onSubmit = { this.handleFormSubmit }>
+          <Form.Field>
+            <label>Event Title</label>
+            <input
+              name='title'
+              value={title}
+              onChange={this.handleInputChange}
+              placeholder='First Name'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input
+              name='date'
+              value={date}
+              onChange={this.handleInputChange}
+              type='date'
+              placeholder='Event Date'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input
+              name='city'
+              value={city}
+              onChange={this.handleInputChange}
+              placeholder='City event is taking place'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input
+              name='venue'
+              value={venue}
+              onChange={this.handleInputChange}
+              placeholder='Enter the Venue of the event'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input
+              name='hostedBy'
+              value={hostedBy}
+              onChange={this.handleInputChange}
+              placeholder='Enter the name of person hosting'
+            />
+          </Form.Field>
+          <Button positive type='submit'>
+            Submit
+          </Button>
+          <Button type='button' onClick={cancelOpenedForm}>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    );
+  }
 }
-export default EventForm
+
+export default EventForm;
